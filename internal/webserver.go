100553c5981b3a08d5a6169b057ff36413194be6
masanjaya <masanjaya@gmail.com>
2022-04-22 11:52:28 +0200

    add dummy vault

--- internal/webserver.go
+++ internal/webserver.go
@@ -2,15 +2,279 @@ package internal
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"reload-config/model"
 	"sync"
+	"time"
 )
 
 // StartHttpServer starts an HTTP Server on port 9090, handle for path / and /reset and /shutdown
 func StartHttpServer(port int, wg *sync.WaitGroup, stop, reset, shutdown chan bool) {
 	defer wg.Done()
+
+	http.HandleFunc("/v1/secret/metadata/pilot/certs", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 listResponse
+		response3.RequestId = "request_id_1"
+		response3.LeaseId = "lease_id_1"
+		response3.Renewable = true
+		response3.LeaseDuration = 10
+		response3.Data.Keys = append(response3.Data.Keys, "empty")
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	http.HandleFunc("/v1/secret/metadata/pilot", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 listResponse
+		response3.RequestId = "request_id_1"
+		response3.LeaseId = "lease_id_1"
+		response3.Renewable = true
+		response3.LeaseDuration = 10
+		response3.Data.Keys = append(response3.Data.Keys, "certs/")
+		response3.Data.Keys = append(response3.Data.Keys, "elstreamer")
+		response3.Data.Keys = append(response3.Data.Keys, "fc")
+		response3.Data.Keys = append(response3.Data.Keys, "tdextract2")
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle list
+	http.HandleFunc("/v1/secret/metadata/", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 listResponse
+		response3.RequestId = "request_id_1"
+		response3.LeaseId = "lease_id_1"
+		response3.Renewable = true
+		response3.LeaseDuration = 10
+		response3.Data.Keys = append(response3.Data.Keys, "pilot/")
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/pilot/elstreamer", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"elstreamer_data_1": "dummy_data_elstreamer1", "elstreamer_data_2": "dummy_data_elstreamer2"}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/pilot/fc", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"fc1": "dummy_data_fc1", "fc2": "dummy_data_fc2"}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/pilot/tdextract2", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"mongo_uri": " mongodb://XXXXXXXXXX@127.0.0.1:27017/FunnelConnectPILOT", "postgresql_uri": "postgres://X@postgres.pilot.teavaro.systems/pilot?sslmode=disable"}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/testkey", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"publickey": model.TestPublicKey, "privatekey": model.TestPrivateKey}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/teavaro", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"publickey": model.SpecificPublicKey1}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/string1", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"publickey": model.PublicKey1, "privatekey": model.PrivateKey1}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /get secret
+	http.HandleFunc("/v1/secret/data/string3", func(rw http.ResponseWriter, r *http.Request) {
+		var response3 secretResponse
+		response3.Data.Metadata.DeletionTime = "never"
+		response3.Data.Metadata.Destroyed = false
+		response3.Data.Metadata.Version = 1
+		response3.Data.Metadata.CreatedTime = time.Now()
+		response3.Data.Data = map[string]string{"publickey": model.PublicKey3, "privatekey": model.PrivateKey3}
+
+		returnbyte, err := json.Marshal(response3)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
+
+	// handle /login
+	http.HandleFunc("/v1/auth/userpass/login/sanjaya@teavaro.com", func(rw http.ResponseWriter, r *http.Request) {
+
+		var requestBody authRequest
+		err := json.NewDecoder(r.Body).Decode(&requestBody)
+		if err != nil {
+			log.Println("can not decode body requset:", err.Error())
+			_, err := rw.Write([]byte("<html>empty body request</html>"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		} else {
+			log.Println("got http request with", requestBody.Password)
+		}
+		log.Println("checking http request done")
+
+		var response2 authResponse
+		response2.LeaseID = "LeaseID"
+		response2.Renewable = true
+		response2.LeaseDuration = 10
+		response2.Auth.ClientToken = "clienttoken1"
+		response2.Auth.Policies = []string{"string1", "string2"}
+		response2.Auth.Metadata.Username = "username1"
+		response2.Auth.LeaseDuration = 2
+		response2.Auth.Renewable = true
+
+		returnbyte, err := json.Marshal(response2)
+		if err != nil {
+			log.Fatal(err)
+			returnbyte = []byte(`{"status":"error"}`)
+		}
+
+		_, err = rw.Write(returnbyte)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	})
 
 	// handle /
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
@@ -52,11 +316,55 @@ func StartHttpServer(port int, wg *sync.WaitGroup, stop, reset, shutdown chan bo
 	}()
 
 	// start serving
-	log.Println("HttpServer is starting")
+	log.Println("HttpServer is starting now")
 	err := httpServer.ListenAndServe()
 	if err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
 
 	log.Println("HttpServer is shutdown")
+}
+
+type authRequest struct {
+	Password string `json:"password"`
+}
+
+type authResponse struct {
+	LeaseID       string `json:"lease_id"`
+	Renewable     bool   `json:"renewable"`
+	LeaseDuration int    `json:"lease_duration"`
+	Auth          struct {
+		ClientToken string   `json:"client_token"`
+		Policies    []string `json:"policies"`
+		Metadata    struct {
+			Username string `json:"username"`
+		} `json:"metadata"`
+		LeaseDuration int  `json:"lease_duration"`
+		Renewable     bool `json:"renewable"`
+	} `json:"auth"`
+}
+
+type secretResponse struct {
+	Data struct {
+		Metadata struct {
+			Version      int       `json:"version"`
+			Destroyed    bool      `json:"destroyed"`
+			DeletionTime string    `json:"deletion_time"`
+			CreatedTime  time.Time `json:"created_time"`
+		} `json:"metadata"`
+		Data map[string]string `json:"data"`
+	} `json:"data"`
+}
+
+type listResponse struct {
+	RequestId     string `json:"request_id"`
+	LeaseId       string `json:"lease_id"`
+	Renewable     bool   `json:"renewable"`
+	LeaseDuration int    `json:"lease_duration"`
+	Data          struct {
+		Keys []string `json:"keys"`
+	} `json:"data"`
+	WrapInfo interface{} `json:"wrap_info"`
+	Warnings interface{} `json:"warnings"`
+	Auth     interface{} `json:"auth"`
 }
--- main.go
+++ main.go
@@ -10,24 +10,24 @@ import (
 )
 
 func main() {
-
+	numberService := 1
 	// channel for stop, reset and shutdown signal
-	stop := make(chan bool, 3)
+	stop := make(chan bool, numberService)
 	reset := make(chan bool)
 	shutdown := make(chan bool)
 
 	// used to synchronize subscriber, publisher and httpserver to finish
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(numberService)
 
 	log.Println("Starting Htpp Server")
-	go internal.StartHttpServer(9090, &wg, stop, reset, shutdown)
+	go internal.StartHttpServer(8300, &wg, stop, reset, shutdown)
 
-	log.Println("Starting Subscriber")
-	go internal.StartRabbitMqSubscriber(&wg, stop)
+	// log.Println("Starting Subscriber")
+	// go internal.StartRabbitMqSubscriber(&wg, stop)
 
-	log.Println("Starting Publisher")
-	go internal.StartRabbitMqPublisher(&wg, stop, reset)
+	// log.Println("Starting Publisher")
+	// go internal.StartRabbitMqPublisher(&wg, stop, reset)
 
 	// waiting for terminate request
 	c := make(chan os.Signal, 1)
@@ -41,7 +41,7 @@ func main() {
 
 	// sending stop to all (Subscriber, Publisher and HttpServer)
 	log.Println("stopping Subscriber, Publisher and HttpServer")
-	for i := 0; i < 3; i++ {
+	for i := 0; i < numberService; i++ {
 		stop <- true
 	}
 
